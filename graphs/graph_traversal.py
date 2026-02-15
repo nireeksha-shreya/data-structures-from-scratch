246afab70195885dbcd15f09a041005a15e18c0e
Nireeksha Alva <shreyanireeksha@gmail.com>
2026-02-15 15:47:08 +0530

    graph_traversal.py

---
+++ graphs/graph_traversal.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+def bfs(start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            queue.extend(graph[node])
+
+
+def dfs(node, visited=None):
+    if visited is None:
+        visited = set()
+
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+
+        for neighbor in graph[node]:
+            dfs(neighbor, visited)
+
+
+print("BFS:")
+bfs('A')
+
+print("\nDFS:")
+dfs('A')
